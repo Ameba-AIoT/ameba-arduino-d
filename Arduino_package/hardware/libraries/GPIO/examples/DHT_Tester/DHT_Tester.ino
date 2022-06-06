a51b446246a4ac1976ce484a8ae3101b211e2def
Daphne <72006538+daphwl@users.noreply.github.com>
2022-06-06 14:19:27 +0800

    Update DHT_Tester.ino comment section (#84)

--- Arduino_package/hardware/libraries/GPIO/examples/DHT_Tester/DHT_Tester.ino
+++ Arduino_package/hardware/libraries/GPIO/examples/DHT_Tester/DHT_Tester.ino
@@ -11,12 +11,12 @@
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Connect the VCC pin of the sensor to 3.3V
+// Connect the DATA pin of the sensor to whatever your DHTPIN is
+// Connect the GND pin of the sensor to GROUND
+// Connect a 1K resistor from DATA pin to VCC pin of the sensor
+
 
 // Initialize DHT sensor.
 // Note that older versions of this library took an optional third parameter to
