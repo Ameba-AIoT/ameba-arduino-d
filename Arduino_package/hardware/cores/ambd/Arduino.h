0b219800f6cc464427e830735d425ef9679036aa
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2022-08-30 11:23:34 +0800

    Pre Release Version 3.1.4
    Version 3.1.4-QC-V02 – 2022/08/30
    
    Feature:
    - Add I2C_Scanner example in Wire
    - Enable lwip non-blocking mode in WiFi
    - Support cpp standard string library
    
    API Updates:
    - Update SPI for wrong pin name mapping
    - Update digital and analog API for modes conflicts with 0x00
    - Update DHT and Wire API
    
    Misc:
    - Update Fritzing and Pinmux
        - Add AW-CU488 Pinmap

--- Arduino_package/hardware/cores/ambd/Arduino.h
+++ Arduino_package/hardware/cores/ambd/Arduino.h
@@ -96,10 +96,10 @@ extern void loop( void );
 // Pin mode 
 // 0x0 to 0x4       "GPIO mode"
 // 0x5 to 0x9       "GPIO_IRQ mode"
-#define MODE_NOT_INITIAL                (1UL<<4)
-#define PWM_MODE_ENABLED                (1UL<<31)
-#define GPIO_MODE_ENABLED               (1UL<<30)
-#define GPIO_IRQ_MODE_ENABLED           (1UL<<29)
+#define MODE_NOT_INITIAL                (1UL<<31)
+#define PWM_MODE_ENABLED                (1UL<<30)
+#define GPIO_MODE_ENABLED               (1UL<<29)
+#define GPIO_IRQ_MODE_ENABLED           (1UL<<28)
 
 /* Types used for the tables below */
 typedef struct _PinDescription
