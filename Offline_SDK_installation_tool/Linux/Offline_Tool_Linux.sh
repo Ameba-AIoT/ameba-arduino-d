7e0fdce3f60ebcebfeeb3fbb69ac470a994135b5
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2022-09-19 11:59:14 +0800

    [Misc] remove unused files

--- Offline_SDK_installation_tool/Linux/Offline_Tool_Linux.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.arduino15/staging/packages/
-cp -r ../../Arduino_package/release/* ~/.arduino15/staging/packages
-cp  ../../Arduino_package/package_realtek.com_amebad_early_index.json ~/.arduino15
-cp  ../../Arduino_package/package_realtek.com_amebad_index.json ~/.arduino15
