713f921dc600ec36d8f916908305dba17535eded
Michael Zhang <35476432+M-ichae-l@users.noreply.github.com>
2023-04-03 14:19:06 +0800

    Update postbuild_img2_arduino_macos.cpp

--- Arduino_package/ameba_d_tools_macos/tools/macos/src/postbuild_img2_arduino_macos.cpp
+++ Arduino_package/ameba_d_tools_macos/tools/macos/src/postbuild_img2_arduino_macos.cpp
@@ -3,6 +3,7 @@
 Compile:
 
 macos:
+g++ -o tools/macos/postbuild_img2_arduino_macos tools/macos/src/postbuild_img2_arduino_macos.cpp
 g++ -mmacosx-version-min=11.0 -o tools/macos/postbuild_img2_arduino_macos tools/macos/src/postbuild_img2_arduino_macos.cpp
 
 */
