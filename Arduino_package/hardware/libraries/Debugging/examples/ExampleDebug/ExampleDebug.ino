e8303acf2a1d5746f98e21d71c699df3cd8304cf
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2023-04-21 15:43:43 +0800

    Update for coding style

--- Arduino_package/hardware/libraries/Debugging/examples/ExampleDebug/ExampleDebug.ino
+++ Arduino_package/hardware/libraries/Debugging/examples/ExampleDebug/ExampleDebug.ino
@@ -17,28 +17,28 @@
 
 int i;
 void setup() {
-  Serial.begin(115200);
-  pinMode(LED_PIN, OUTPUT);
-  for (i = 0; i < 10; i++) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
-  }
-  i = 0;
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+    for (i = 0; i < 10; i++) {
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(LED_PIN, LOW);
+        delay(100);
+    }
+    i = 0;
 }
 
 void loop() {
-  i++;
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  if ((i%10)==0) {
-    Serial.println(i);
-    //RTL8722 only supports a maximum of 2 hardware breakpoints. If you need more breakpoints, you can use either of the instructions below to add a software breakpoint in program execution
-    //Note: To continue program execution from a software breakpoint, use "Step Over" first before "Continue"
-    //__BKPT();
-    __asm__("BKPT");
-  }
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+    i++;
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    if ((i % 10) == 0) {
+        Serial.println(i);
+        //RTL8722 only supports a maximum of 2 hardware breakpoints. If you need more breakpoints, you can use either of the instructions below to add a software breakpoint in program execution
+        //Note: To continue program execution from a software breakpoint, use "Step Over" first before "Continue"
+        //__BKPT();
+        __asm__("BKPT");
+    }
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
 }
