27a72272061e7de2f521c9f6c3f7b6a2000ee0e1
zhangzhenwu <zhangzhenwu@realtek-sg.com>
2023-06-30 16:00:49 +0800

    Update UDP_CalculateSendDelay.ino

--- Arduino_package/hardware/libraries/WiFi/examples/UDP_Calculation/UDP_CalculateSendDelay/UDP_CalculateSendDelay.ino
+++ Arduino_package/hardware/libraries/WiFi/examples/UDP_Calculation/UDP_CalculateSendDelay/UDP_CalculateSendDelay.ino
@@ -14,8 +14,8 @@ int keyIndex = 0;                   // your network key Index number (needed onl
 int status = WL_IDLE_STATUS;        // Indicater of Wifi status
 
 WiFiUDP Udp;
-char server[] ="0.0.0.0";
-String str_server;
+char client_ip[] ="0.0.0.0";
+String str_client_ip;
 int port = 5001;
 
 void setup() {
@@ -40,17 +40,17 @@ void setup() {
 
     Serial.print("Enter client IP address, ");
     while (Serial.available() == 0);
-    str_server = Serial.readString();
-    str_server.trim();
-    str_server.toCharArray(server, (str_server.length() + 1));
-    Serial.println(server);
+    str_client_ip = Serial.readString();
+    str_client_ip.trim();
+    str_client_ip.toCharArray(client_ip, (str_client_ip.length() + 1));
+    Serial.println(client_ip);
     delay(5000);
 }
 
 char buf[256];
 void loop() {
     sprintf(buf, "%d", ((int)(millis())));
-    Udp.beginPacket(server, port);
+    Udp.beginPacket(client_ip, port);
     Udp.write(buf);
     Udp.endPacket();
     delay(5);
