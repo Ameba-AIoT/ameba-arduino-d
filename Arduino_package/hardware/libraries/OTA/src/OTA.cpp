f746876971f8aadad4ae4573e04719e089795ae8
Kyderio <71754932+Kyderio@users.noreply.github.com>
2024-03-25 16:12:28 +0800

    Bug Fix for OTA BW16, Fix is by CSDN user (#227)
    * Bug Fix for OTA BW16, Fix is by CSDN user
    
    * Update OTA.cpp

--- Arduino_package/hardware/libraries/OTA/src/OTA.cpp
+++ Arduino_package/hardware/libraries/OTA/src/OTA.cpp
@@ -12,7 +12,8 @@ OTA::OTA(){};
 
 OTA::~OTA(){};
 
-void OTA::beginOTA(int port) {
+void OTA::beginOTA(int port)
+{
     int server_socket = -1, client_socket = -1;
     unsigned char *alloc = NULL;
     int alloc_buf_size = BUF_SIZE;
@@ -40,18 +41,23 @@ void OTA::beginOTA(int port) {
     }
     syncOTAAddr(ota_target_index);
 
+    if (ota_get_cur_index() == OTA_INDEX_1) {
+        ota_target_index = OTA_INDEX_2;
+    } else {
+        ota_target_index = OTA_INDEX_1;
+    }
 
     /*--------------------step2: receive firmware information-----------------*/
     /* Receive file_info[] from server. */
     memset(file_info, 0, sizeof(file_info));
-    if(!recv_file_info_from_server((u8 *)file_info, sizeof(file_info), client_socket)) {
+    if (!recv_file_info_from_server((u8 *)file_info, sizeof(file_info), client_socket)) {
         printf("[%s] [ERROR] Receive firmware file_info failed\n", __FUNCTION__);
         goto update_ota_exit;
     }
     printf("[%s] Receive file_info done\n", __FUNCTION__);
-    
+
     /* Receive firmware header */
-    if(!recv_ota_file_hdr(alloc, &RevHdrLen, &OtaTargetHdr, client_socket)) {
+    if (!recv_ota_file_hdr(alloc, &RevHdrLen, &OtaTargetHdr, client_socket)) {
         printf("[%s] [ERROR] Receive firmware header failed", __FUNCTION__);
         goto update_ota_exit;
     }
@@ -59,7 +65,7 @@ void OTA::beginOTA(int port) {
 
 
     /* -----step3: parse firmware header and get target OTA image header-----*/
-    if(!get_ota_tartget_header(alloc, RevHdrLen, &OtaTargetHdr, ota_target_index)) {
+    if (!get_ota_tartget_header(alloc, RevHdrLen, &OtaTargetHdr, ota_target_index)) {
         printf("[%s] [ERROR] Get OTA header failed\n", __FUNCTION__);
         goto update_ota_exit;
     }
@@ -68,50 +74,55 @@ void OTA::beginOTA(int port) {
 
     /*-----------------step4: erase flash space for new firmware-------------*/
     printf("\tErase is ongoing...\n");
-    for(i = 0; i < OtaTargetHdr.ValidImgCnt; i++) {
-        erase_ota_target_flash(OtaTargetHdr.FileImgHdr[i].FlashAddr, OtaTargetHdr.FileImgHdr[i].ImgLen);
-    }
-    printf("[%s] Erase flash for new firmware done\n", __FUNCTION__);
+    // for(i = 0; i < OtaTargetHdr.ValidImgCnt; i++) {
+    //     erase_ota_target_flash(OtaTargetHdr.FileImgHdr[i].FlashAddr, OtaTargetHdr.FileImgHdr[i].ImgLen);
+    // }
+    // printf("[%s] Erase flash for new firmware done\n", __FUNCTION__);
 
+    if (ota_target_index == 1) {
+        erase_ota_target_flash(0x08106000, OtaTargetHdr.FileImgHdr[0].ImgLen);
+    } else {
+        erase_ota_target_flash(0x08006000, OtaTargetHdr.FileImgHdr[0].ImgLen);
+    }
 
     /*---------step5: download new firmware from server and write it to flash--------*/
-    if(download_new_fw_from_server(client_socket, &OtaTargetHdr, ota_target_index) == _FALSE){
+    if (download_new_fw_from_server(client_socket, &OtaTargetHdr, ota_target_index) == _FALSE) {
         printf("[%s] [ERROR] Download new firmware failed\n", __FUNCTION__);
         goto update_ota_exit;
     }
     printf("[%s] Download new firmware done\n", __FUNCTION__);
 
 
-     /*-------------step6: verify checksum and update signature-----------------*/
-    if(verify_ota_checksum(&OtaTargetHdr)){
-        if(!change_ota_signature(&OtaTargetHdr, ota_target_index)) {
+    /*-------------step6: verify checksum and update signature-----------------*/
+    if (verify_ota_checksum(&OtaTargetHdr)) {
+        if (!change_ota_signature(&OtaTargetHdr, ota_target_index)) {
             printf("[%s] [ERROR] Update OTA signature failed\n", __FUNCTION__);
             goto update_ota_exit;
         }
         printf("[%s] Update OTA signature done\n", __FUNCTION__);
         ret = 0;
-    } 
+    }
 
 
     /*-------------step7: OTA upgrade successfully, restart device------------*/
-    if(!ret){
-        printf("[%s] Ready to reboot\n", __FUNCTION__);	
+    if (!ret) {
+        printf("[%s] Ready to reboot\n", __FUNCTION__);
         ota_platform_reset();
     }
 
-    update_ota_exit:
-    if(alloc){
+update_ota_exit:
+    if (alloc) {
         ota_update_free(alloc);
     }
-    if(server_socket >= 0 || client_socket >=0){
+    if (server_socket >= 0 || client_socket >= 0) {
         close_socket(server_socket);
         close_socket(client_socket);
-    }	
+    }
     printf("[%s] Update task exit\n", __FUNCTION__);
-
 }
 
-void OTA::syncOTAAddr(u32 ota_target_index){
+void OTA::syncOTAAddr(u32 ota_target_index)
+{
     /* check OTA index we should update */
     if (ota_get_cur_index() == OTA_INDEX_1) {
         ota_target_index = OTA_INDEX_2;
